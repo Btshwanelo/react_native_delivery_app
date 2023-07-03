281b3d2febf0174c05e60a35ab7b3d3780447a80
Btshwanelo <48760912+Btshwanelo@users.noreply.github.com>
2023-07-03 16:00:18 +0200

    refactored

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,67 @@
-import React from 'react';
-import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useLayoutEffect } from 'react';
+import { SafeAreaView, View, Text, Image, TextInput, ScrollView } from 'react-native';
+import { ChevronDownIcon, UserIcon, AdjustmentsVerticalIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline';
 
 const HomeScreen = () => {
-  return <View>HomeScreen</View>;
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView className='bg-white pt-5'>
+      <Text className=''>
+        {/*Header*/}
+        <View className=' flex flex-row pb-3 items-center mx-4 space-x-2 px-1'>
+          <Image
+            source={{
+              uri: 'https://links.papareact.com/wru',
+            }}
+            className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+          />
+          <View className='flex-1'>
+            <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
+            <Text className='font-bold text-xl'>
+              Current Location
+              <ChevronDownIcon
+                size={20}
+                color='#00CCBB'
+              />
+            </Text>
+          </View>
+          <UserIcon
+            size={35}
+            color='#00CCBB'
+          />
+        </View>
+        {/*Search*/}
+        <View className=' flex flex-row items-center space-x-2 pb-2 mx-4 px-1'>
+          <View className='flex-row space-x-2 flex-1 bg-gray-200 p-3'>
+            <MagnifyingGlassIcon
+              color='gray'
+              size={20}
+            />
+            <TextInput
+              placeholder='Resturants and cusines'
+              keyboardType='default'
+            />
+          </View>
+          <AdjustmentsVerticalIcon color='#00CCBB' />
+        </View>
+
+        {/*Body*/}
+        <ScrollView>
+          {/*Categoriees */}
+
+          {/*Featured Rows */}
+        </ScrollView>
+      </Text>
+    </SafeAreaView>
+  );
 };
 
 export default HomeScreen;
